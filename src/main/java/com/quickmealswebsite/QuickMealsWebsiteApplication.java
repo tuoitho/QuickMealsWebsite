fa4cf44aa01e853d69eb4dab8fa8771d2d4c898a
tuoitho <tuoithokhoc1414@gmail.com>
2024-09-04 16:07:41 +0700

    init1

---
+++ src/main/java/com/quickmealswebsite/QuickMealsWebsiteApplication.java
@@ -0,0 +1,20 @@
+package com.quickmealswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickMealsWebsiteApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(QuickMealsWebsiteApplication.class, args);
+//        SpringApplication.run(QuickMealsWebsiteApplication.class, args);
+
+
+
+
+    }
+
+
+}
